c77c0e1fb21234f219a791b74a4a99dd9c94963c
Adriano <adriano.marchi@premier.soft.net>
2021-11-30 11:36:21 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioHttpClient/Program.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ExercicioHttpClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Iniciando consulta de endereço...");
+
+            var client = new HttpClient();
+            var jsonOptions = new JsonSerializerSettings()
+            {
+                Formatting = Formatting.Indented,
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            };
+
+            try
+            {
+                var res = await client.GetAsync("https://viacep.com.br/ws/89041710/json/");
+
+                if (res.IsSuccessStatusCode)
+                {
+                    var jsonEntrada = await res.Content.ReadAsStringAsync();
+                    var endereco = JsonConvert. DeserializeObject<Endereco>(jsonEntrada,jsonOptions);  
+
+                    Console.WriteLine("Json recebido:");
+                    Console.WriteLine(jsonEntrada);
+
+                    var usuario = new Usuario("Adriano","Marchi","065.345.219-23");
+                    usuario.AdicionarEndereco(endereco);
+
+                    var jsonSaida = JsonConvert.SerializeObject(usuario, jsonOptions);
+                    Console.WriteLine("Json a ser enviado:");
+                    Console.WriteLine(jsonSaida);
+
+                }
+                else
+                {
+                    Console.WriteLine("Não foi possível consultar o endereço: " + res.StatusCode);
+                }
+
+            }catch (Exception ex)
+            {
+                Console.WriteLine("Erro ao consultar endereço: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ ExercicioHttpClient/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioHttpClient
+{
+    public class Usuario
+    {
+        public Usuario(string nome,
+                       string sobreNome,
+                       string documento)
+        {
+            Nome = nome;
+            SobreNome = sobreNome;
+            Documento = documento;
+        }
+
+        public string Nome { get; private set; }
+        public string SobreNome { get; private set; }
+        public string Documento { get; private set; }
+        public Endereco Endereco { get; private set; }
+
+        public void AdicionarEndereco(Endereco endereco)
+        {
+            Endereco = endereco; 
+        }
+
+    }
+}
