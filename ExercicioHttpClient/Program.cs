adece37e7d71b4fde7539b4cb0a048d9b191d066
Adriano <adriano.marchi@premier.soft.net>
2021-11-30 13:53:25 -0300

    Terceiro Comitt

--- ExercicioHttpClient/Program.cs
+++ ExercicioHttpClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Polly;
+using Newtonsoft.Json;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -12,42 +13,128 @@ namespace ExercicioHttpClient
             Console.WriteLine("Iniciando consulta de endereço...");
 
             var client = new HttpClient();
+            client.Timeout = TimeSpan.FromSeconds(1);
+
+            var maxRetryAttemps = 5;
+            var pauseBetweenFAilures = TimeSpan.FromSeconds(1);
+            
             var jsonOptions = new JsonSerializerSettings()
             {
                 Formatting = Formatting.Indented,
-                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
+                
             };
 
-            try
+            var retryPolly = Policy
+                .Handle<HttpRequestException>()
+                .WaitAndRetryAsync(maxRetryAttemps, i => pauseBetweenFAilures);
+
+            string[] myAdress = { "89051-300", "89010-000", "99999999", "12345678977", "89041-710" };
+
+            //Posso fazer uma requisação com retry dessa forma também:
+            //var response = await GetAsync("https://www.google.com.br", 5);
+            //dessa forma no response vou ter o conteúdo para poder deserializar. Essa forma é usada se eu não quiser importar uma biblioteca como o polly, por exemplo. Ai
+            // eu faço o polly de forma manual.
+
+
+            foreach (var c in myAdress)
             {
-                var res = await client.GetAsync("https://viacep.com.br/ws/89041710/json/");
-
-                if (res.IsSuccessStatusCode)
+                try
                 {
-                    var jsonEntrada = await res.Content.ReadAsStringAsync();
-                    var endereco = JsonConvert. DeserializeObject<Endereco>(jsonEntrada,jsonOptions);  
+                    await retryPolly.ExecuteAsync(async () =>
+                    {
+                        var res = await client.GetAsync("https://viacep.com.br/ws/" + c + "/json/");
+                        //res.EnsureSuccessStatusCode();
 
-                    Console.WriteLine("Json recebido:");
-                    Console.WriteLine(jsonEntrada);
+                        if (res.IsSuccessStatusCode)
+                        {
+                            var jsonEntrada = await res.Content.ReadAsStringAsync();
 
-                    var usuario = new Usuario("Adriano","Marchi","065.345.219-23");
-                    usuario.AdicionarEndereco(endereco);
+                            if (jsonEntrada.Contains("erro"))
+                            {
+                                throw new Exception("CEP consultado é inválido!");
+                            }
 
-                    var jsonSaida = JsonConvert.SerializeObject(usuario, jsonOptions);
-                    Console.WriteLine("Json a ser enviado:");
-                    Console.WriteLine(jsonSaida);
+                            var adress = JsonConvert.DeserializeObject<Endereco>(jsonEntrada, jsonOptions);
+
+                            Console.WriteLine("Json recebido:");
+                            Console.WriteLine(jsonEntrada);
+
+                            var user = new Usuario("Adriano", "Marchi", "065.345.219-23");
+                            user.AdicionarEndereco(adress);
+
+                            var jsonSaida = JsonConvert.SerializeObject(user, jsonOptions);
+                            Console.WriteLine("Json a ser enviado:");
+                            Console.WriteLine(jsonSaida);
+
+                        }
+                        else
+                        {
+                            switch ((int)res.StatusCode)
+                            {
+                                case 400:
+                                    Console.WriteLine("Requisição inválida!");
+                                    break;
+                                case 404:
+                                    Console.WriteLine("Página não encontrada!");
+                                    break;
+                                case 500:
+                                    Console.WriteLine("Erro interno no servidor!");
+                                    break;
+                                default:
+                                    Console.WriteLine("Não foi possível consultar o endereço: " + (int)res.StatusCode + " -> " + res.StatusCode);
+                                    break;
+
+                            }
+
+                        }
+
+                    });
 
                 }
-                else
+                catch (Exception ex)
                 {
-                    Console.WriteLine("Não foi possível consultar o endereço: " + res.StatusCode);
+                    Console.WriteLine("Erro ao consultar endereço: " + ex.Message);
                 }
 
-            }catch (Exception ex)
-            {
-                Console.WriteLine("Erro ao consultar endereço: " + ex.Message);
             }
+        }
+        
+        public static async Task<string> GetAsync(string url, int retryCount)
+        {
+            var client = new HttpClient();
+            var response = string.Empty;
+            var retry = false;
+            var retryIndex = 0;
+
+            do
+            {
+                Console.WriteLine("Fazendo a requisição {0} de 5", retryIndex);
+                var res = await client.GetAsync(url);
+                if (!res.IsSuccessStatusCode)
+                {
+                    retry = true;
+                    retryIndex++;
+                }
+
+                response = await res.Content.ReadAsStringAsync();
+                
+
+            } while (retry && retryIndex < retryCount);
+
+            return response;
 
         }
-    }
+    }    
+
 }
+
+
+
+
+ 
+
+ 
+ 
+ 
+ 
